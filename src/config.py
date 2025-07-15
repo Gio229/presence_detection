151213653e0a10d92d762217b8b88b1218d3ebd7
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2025-07-15 18:40:28 +0100

    set class name into configs

--- src/config.py
+++ src/config.py
@@ -2,3 +2,14 @@ KAFKA_BROKER = "localhost:9092"
 # KAFKA_BROKER = "kafka:29092"
 KAFKA_CLASS_DETECTION_STREAM_TOPIC = "class_detection_stream"
 MESSAGE_INTERVAL_SECONDS = 3
+YOLO_CLASS_NAMES = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                    "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                    "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                    "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                    "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                    "teddy bear", "hair drier", "toothbrush"
+                    ]
